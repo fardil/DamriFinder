1837f8856abbc4229dd46dcb99f89acc4eeed67f
rividya <ivy.aluna@gmail.com>
2019-11-19 20:51:45 +0700

    splash update

---
+++ app/src/main/java/com/dilla/damrifinder/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.dilla.damrifinder.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dilla.damrifinder.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
