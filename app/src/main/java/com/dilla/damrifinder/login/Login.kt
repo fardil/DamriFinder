144cad333ae03ae9fbbd8bba8229d1f1fff3563d
fardil <faradillaazra@gmail.com>
2019-11-19 15:33:00 +0700

    login update

--- app/src/main/java/com/dilla/damrifinder/login/Login.kt
+++
@@ -1,2 +0,0 @@
-package com.dilla.damrifinder.login
-
---
+++ app/src/main/java/com/dilla/damrifinder/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.dilla.damrifinder.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dilla.damrifinder.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
